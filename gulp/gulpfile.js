fa1494b8f3c52c04976e2f69db789eb8c30786bc
frontYang <1769627503@qq.com>
2018-03-19 18:32:33 +0800

    Update gulpfile.js
    fix  browserSync

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -16,45 +16,70 @@ const imagemin = require('gulp-imagemin'); // 优化图片
 const spritesmith = require('gulp.spritesmith'); // 图片精灵
 const cache = require('gulp-cache'); // 缓存代理任务。，减少图片重复压缩
 const del = require('del'); // 清理生成文件
+const config = { // 路径
+  baseDir: 'G:/www/work/pcauto/2018/cheshili/pc/dist',
+  cssDir: {
+    src: 'src/css/**/*.scss',
+    dest: 'dist/css'
+  },
+  jsDir: {
+    src: 'src/js/**/*.js',
+    dest: 'dist/js'
+  },
+  htmlDir: {
+    src: 'src/*.html',
+    src_all: 'src/**/*.html',
+    dest: 'dist/'
+  },
+  imgDir: {
+    src: 'src/images/*.png',
+    dest: 'dist/images',
+    src_icon: 'src/images/icons/*.png',
+    dest_icon: 'dist/'
+  }
+}
 
 
 gulp.task('browserSync', function(){
-	browserSync({
-		server: {
-			baseDir: 'dist'
-		}
-	})
+  browserSync({
+    server: {
+      baseDir: config.baseDir,
+      open: false
+    }
+  })
 })
 
 
 gulp.task('sass', function(){
-	return gulp.src('src/css/**/*.scss')
-	.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
-  .pipe(dgbl())
-  .pipe(postcss([autoprefixer({browsers: ['last 2 versions', 'Android > 4.4','iOS >= 8', 'Firefox >= 20']})]))
-	.pipe(gulp.dest('dist/css'))
-	.pipe(browserSync.reload({  // 只有被改变的地方局部刷新
-		stream: true
-	}))
+	return gulp.src(config.cssDir.src)
+  	.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+    .pipe(dgbl())
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions', 'Android > 4.4','iOS >= 8', 'Firefox >= 20', 'ie >= 7']})]))
+  	.pipe(gulp.dest(config.cssDir.dest))
+  	.pipe(reload({ stream: true}))
 })
 
+
 gulp.task('babel', function(){
-	return gulp.src('src/js/**/*.js')
-	.pipe(babel({
-		presets: ['@babel/env'],
-		plugins: ['@babel/transform-runtime']
-	}))
-	// .pipe(uglify())
-	.pipe(gulp.dest('dist/js'))
+	return gulp.src(config.jsDir.src)
+  	.pipe(babel({
+  		presets: ['@babel/env'],
+  		plugins: ['@babel/transform-runtime']
+  	}))
+  	// .pipe(uglify())
+  	.pipe(gulp.dest(config.jsDir.dest))
+    .pipe(reload({ stream: true}))
 })
 
 gulp.task('concat',function() {
-    gulp.src('src/*.html')
-        .pipe(contentIncluder({
-            includerReg:/<!\-\-\#include\s+virtual="([^"]+)"\-\->/g
-        }))
-        .pipe(gulp.dest('dist/'))
+  return gulp.src(config.htmlDir.src)
+    .pipe(contentIncluder({
+        includerReg:/<!\-\-\#include\s+virtual="([^"]+)"\-\->/g
+    }))
+    .pipe(gulp.dest(config.htmlDir.dest))
+    .pipe(reload({ stream: true}))
 });
+
 
 // gulp.task('useref', function() {
 //   return gulp.src('src/*.html')
@@ -63,43 +88,30 @@ gulp.task('concat',function() {
 //     .pipe(gulpIf('*.css', cssnano()))
 //     .pipe(gulp.dest('dist'));
 // });
-gulp.task('images', function() {
-  return gulp.src('src/images/icon/*.png')
+gulp.task('spritesmith', function() {
+  return gulp.src(config.imgDir.src_icon)
     // Caching images that ran through imagemin
     .pipe(cache(imagemin({
       interlaced: true,
     })))
     .pipe(spritesmith({
-        imgName:'images/sprite.png', //合并后大图的名称
-        cssName:'css/block/sprite.css',
+        imgName:'images/strength_icon.png', //合并后大图的名称
+        cssName:'css/block/strength_icon.css',
         padding:2// 每个图片之间的间距，默认为0px
-        // cssTemplate:(data)=>{
-        // // data为对象，保存合成前小图和合成打大图的信息包括小图在大图之中的信息
-        //    let arr = [],
-        //         width = data.spritesheet.px.width,
-        //         height = data.spritesheet.px.height,
-        //         url =  data.spritesheet.image
-        //     // console.log(data)
-        //     data.sprites.forEach(function(sprite) {
-        //         arr.push(
-        //             '.icon-'+sprite.name+
-        //             '{'+
-        //                 'background: url('+url+') '+
-        //                 'no-repeat '+
-        //                 sprite.px.offset_x+' '+sprite.px.offset_y+';'+
-        //                 'background-size: '+ width+' '+height+';'+
-        //                 'width: '+sprite.px.width+';'+
-        //                 'height: '+sprite.px.height+';'+
-        //             '}\n'
-        //         )
-        //     })
-        //     // return "@fs:108rem;\n"+arr.join("")
-        //     return arr.join('')
-        // }
     }))
 
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest(config.imgDir.dest_icon))
 });
+
+gulp.task('images', function() {
+  return gulp.src(config.imgDir.src)
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+
+    .pipe(gulp.dest(config.imgDir.dest))
+});
+
 
 gulp.task('clean:dist', function() {
   return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
@@ -117,19 +129,27 @@ gulp.task('build', function(callback) {
     'babel',
     'concat',
     'images',
-    // ['useref', 'images'],
+    'spritesmith',
     callback
   )
 })
+})
 
 gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/**/*.html', browserSync.reload);
-  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch(config.cssDir.src, ['sass']);
+  gulp.watch(config.jsDir.src, ['babel']);
+  gulp.watch(config.htmlDir.src_all, ['concat']);
 })
 
 gulp.task('default', function(callback) {
-  runSequence(['clean:dist','sass', 'babel', 'browserSync', 'concat', 'images'], 'watch',
+  runSequence([
+    'clean:dist',
+    'sass',
+    'babel',
+    'concat',
+    'images',
+    'spritesmith',
+    'browserSync'], 'watch',
     callback
   )
 })
