94c35475f4a9f434a898cc139c99e8ec3f391292
yangzhifang <1769627503@qq.com>
2018-10-12 09:46:18 +0800

    vue+webpack

---
+++ vue/webpack+vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'dddddd'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "./styles/index.less";
+
+</style>
+
---
+++ vue/webpack+vue/src/components/layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui-layout">
+    <div v-for="(item, index) in colNum" :key="index" :class="'ui-col-' + colNum / 2"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'ui-layout',
+      msg: 'aaa'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
